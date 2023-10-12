bef31f7bdf7b1f88ac666b31cceb7b546cd30dd2
nayeer <nayeer.naushad786@gmail.com>
2023-10-12 22:25:33 +0530

    added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553        //value cant be changed in const
+let accountEmail = "naiyerdx786@gmail.com"   //to declare variable we either use let or var but in today lifestyle we dont use var reason is giuven in curly{} braket
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2  //not allowed
+
+
+accoutEmail ="ha@gc.gmail.com"
+accountPassword = "21012102"
+accountCity = "Bengaluru"
+console.log(accountId);
+
+/*Prefer not to use var
+we can sometime use js without variable but it is not good*/
+
+console.log([accountId,accountEmail,accountPassword,accountCity,accountState]);
