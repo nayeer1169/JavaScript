9bb293daff9d4ec2080ad29ad73dec1ba6fbe750
nayeer <nayeer.naushad786@gmail.com>
2023-10-23 01:06:07 +0530

    07_memory

---
+++ 01_basics/07_memory.js
@@ -0,0 +1,20 @@
+//Stack Memory (primitive type)
+let myName = "nayeerr"
+let anotherName = "king"
+anotherName="kohli"
+console.log(myName);
+console.log(anotherName);
+
+// eg:
+let userOne = {
+    email:"abc@gmail.com",
+    upi:"user@sbi"
+}  
+
+let userTwo=userOne
+
+userTwo.email="abcd@gmail.com"
+console.log(userOne,email);
+console.log(userTwo,email);
+
+//heap memory (no primitive type)
